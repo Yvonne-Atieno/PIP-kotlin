8820b2a6aac2f055d33132a01873e318876bd537
Yvonne-Atieno <oyvonneatieno@gmail.com>
2023-04-12 02:59:13 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,34 @@
+//create variable name carName and assign the value pickup to it
+fun main(){
+    carName()
+    myFunction()
+    name1()
+    name()
+
+}
+fun carName()  {
+    var a="pickup"
+    println(a)
+}
+
+//insert the missing part to create function with the name myFunction
+fun myFunction(){
+    var b="I just got executed"
+    println(b)
+}
+
+//create variable name myNum assign 50 to it.
+fun name1(){
+    var myNum=50
+    println(myNum)
+}
+
+//create variable called z assign x+y to it and display the result.
+fun name(){
+    var y=5
+    var x=6
+    var z=x+y
+    println(z)
+
+
+}
